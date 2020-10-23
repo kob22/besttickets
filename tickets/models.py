1a8187bd2fa938bb5e455ef80956bad369892647
kob22 <kob2222@gmail.com>
2020-10-23 14:32:15 +0200

    event model

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=300, validators=[MinLengthValidator(5)])
+    date_event = models.DateTimeField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
