ea010581a84af06b33e9d4f52b90a6eb73d0bf71
kob22 <kob2222@gmail.com>
2020-10-26 15:15:12 +0100

    ticket serializer

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -7,3 +7,9 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Event
         fields = ("id", "name", "date_event")
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Ticket
+        fields = ("id", "event", "category", "price", "qty")
