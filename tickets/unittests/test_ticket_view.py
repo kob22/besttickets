4cf040a65c4665a2779d9fba4b780ad0dc303ada
kob22 <kob2222@gmail.com>
2020-10-26 18:47:39 +0100

    tests for tickets view

---
+++ tickets/unittests/test_ticket_view.py
@@ -0,0 +1,94 @@
+import json
+
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.renderers import JSONRenderer
+from rest_framework.test import APIRequestFactory
+
+from tickets.models import Ticket
+from tickets.serializers import TicketSerializer
+from tickets.views import TicketDetailView, TicketListView
+
+
+### There is only information about get tickets details in documentation, so tests are only for get views.
+class TicketViewsALLTest(TestCase):
+
+    fixtures = [
+        "tickets/unittests/fixtures/events.json",
+        "tickets/unittests/fixtures/two_events_four_tickets.json",
+    ]
+
+    def test_get_all_tickets_from_event_no_tickets(self):
+        factory = APIRequestFactory()
+        tickets_view = TicketListView.as_view()
+        request = factory.get(reverse("tickets-for-event-list", args=(1,)))
+        response = tickets_view(request, event_id=1)
+        response.render()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content), [])
+        self.assertEqual(response["content-type"], "application/json")
+
+    def test_get_all_tickets_from_wrong_id_event(self):
+        factory = APIRequestFactory()
+        tickets_view = TicketListView.as_view()
+        request = factory.get(reverse("tickets-for-event-list", args=(120,)))
+        response = tickets_view(request, event_id=120)
+        response.render()
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(json.loads(response.content), {"detail": "Not found."})
+        self.assertEqual(response["content-type"], "application/json")
+
+    def test_get_all_tickets_from_event(self):
+        event_id = 2
+        factory = APIRequestFactory()
+        tickets_view = TicketListView.as_view()
+        request = factory.get(reverse("tickets-for-event-list", args=(event_id,)))
+        response = tickets_view(request, event_id=event_id)
+        response.render()
+
+        tickets_serialized = TicketSerializer(
+            Ticket.objects.filter(event=event_id), many=True
+        ).data
+        tickets_json = JSONRenderer().render(tickets_serialized)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.content, tickets_json)
+        self.assertEqual(response["content-type"], "application/json")
+
+
+class TicketViewsDetailTest(TestCase):
+
+    fixtures = [
+        "tickets/unittests/fixtures/events.json",
+        "tickets/unittests/fixtures/two_events_four_tickets.json",
+    ]
+
+    def test_get_non_existing_tickets(self):
+        factory = APIRequestFactory()
+        ticket_view = TicketDetailView.as_view()
+        request = factory.get(reverse("ticket-detail", args=(222,)))
+        response = ticket_view(request, ticket_id=222)
+        response.render()
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(json.loads(response.content), {"detail": "Not found."})
+        self.assertEqual(response["content-type"], "application/json")
+
+    def test_get_existing_ticket(self):
+        ticket_id = 3
+        factory = APIRequestFactory()
+        ticket_view = TicketDetailView.as_view()
+        request = factory.get(reverse("ticket-detail", args=(ticket_id,)))
+        response = ticket_view(request, ticket_id=ticket_id)
+        response.render()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.content,
+            JSONRenderer().render(
+                TicketSerializer(Ticket.objects.get(pk=ticket_id)).data
+            ),
+        )
+        self.assertEqual(response["content-type"], "application/json")
