7b620d287b388ed48866ffcf0d0edc35e848b46b
kob22 <kob2222@gmail.com>
2020-10-27 19:56:50 +0100

    order and ticket model

--- tickets/models.py
+++ tickets/models.py
@@ -1,5 +1,8 @@
+from datetime import timedelta
+
 from django.core.validators import MinLengthValidator
-from django.db import models
+from django.db import IntegrityError, models, transaction
+from django.utils.timezone import now
 
 
 class Event(models.Model):
@@ -14,3 +17,44 @@ class TicketType(models.Model):
     price = models.DecimalField(blank=False, max_digits=8, decimal_places=2)
     qty = models.IntegerField(blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    expired_at = models.DateTimeField()
+    total = models.DecimalField(blank=False, default=0, max_digits=8, decimal_places=2)
+    paid = models.CharField(max_length=1, default="N")
+    paid_date = models.DateTimeField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        d = timedelta(minutes=15)
+
+        if not self.id:
+            self.expired_at = now() + d
+            super().save(*args, **kwargs)
+
+
+class Ticket(models.Model):
+    type = models.ForeignKey(
+        TicketType,
+        related_name="Tickets",
+        related_query_name="Ticket",
+        on_delete=models.PROTECT,
+    )
+    status = models.CharField(max_length=1, blank=False, default="R")
+    order = models.ForeignKey(
+        Order,
+        related_name="Tickets",
+        related_query_name="Ticket",
+        on_delete=models.PROTECT,
+    )
+
+    # def save(self, *args, **kwargs):
+    #     if self.pk:
+    #         super().save(*args, **kwargs)
+    #     else:
+    #
+    #         if Ticket.objects.filter(type=self.type).count() < self.type.qty:
+    #             super().save(*args, **kwargs)  # Call the "real" save() method.
+    #         else:
+    #             raise Exception('too much tickets')
---
+++ tickets/unittests/test_order.py
@@ -0,0 +1,28 @@
+import datetime
+from decimal import Decimal
+from unittest import mock
+
+import pytz
+from django.core.exceptions import ValidationError
+from django.db.utils import DataError
+from django.test import TestCase
+
+from besttickets.settings import REST_FRAMEWORK
+from tickets.models import Order
+from tickets.serializers import EventSerializer
+
+
+class OrderModelTest(TestCase):
+    def test_create_order_model(self):
+        date_to_mock = datetime.datetime(2020, 1, 1, 0, 0, 0, tzinfo=pytz.utc)
+        with mock.patch(
+            "django.utils.timezone.now", mock.Mock(return_value=date_to_mock)
+        ):
+            order = Order(total=1000)
+            order.save()
+        order = Order.objects.get(pk=1)
+        self.assertEqual(order.created_at, date_to_mock)
+        # self.assertEqual(order.expired_at, date_to_mock + datetime.timedelta(minutes=15) )
+        self.assertEqual(order.total, Decimal("1000.00"))
+        self.assertEqual(order.paid, "N")
+        self.assertEqual(order.paid_date, None)
